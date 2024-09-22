851845d1b1a7ad5be164ada3fdf4a4d646c77b62
rappire <rappire@gmail.com>
2024-09-23 00:38:45 +0900

    feat: 테스트를 위해 JpaAuditing 분리

---
+++ src/main/java/com/swyp6/familytravel/common/config/JpaAuditingConfig.java
@@ -0,0 +1,9 @@
+package com.swyp6.familytravel.common.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaAuditingConfig {
+}
