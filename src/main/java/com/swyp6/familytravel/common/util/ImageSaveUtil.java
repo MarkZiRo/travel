2002bfef53660569683231e1112c3103a7b38cc3
rappire <rappire@gmail.com>
2024-09-23 00:38:01 +0900

    refactor: 이미지 저장 기능 분리, 및 사용하지 않는 컬럼 제거

---
+++ src/main/java/com/swyp6/familytravel/common/util/ImageSaveUtil.java
@@ -0,0 +1,60 @@
+package com.swyp6.familytravel.common.util;
+
+import com.swyp6.familytravel.image.entity.Image;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.LocalDate;
+
+@Component
+public class ImageSaveUtil {
+
+    @Value("${images.upload-root}")
+    private String uploadRoot;
+
+    private Path getImageSaveDir(String fileName) {
+        Path directoryPath = Paths.get(uploadRoot, LocalDate.now().toString());
+        if(Files.notExists(directoryPath)){
+            try{
+                Files.createDirectory(directoryPath);
+            }catch (IOException e){
+                throw new RuntimeException("이미지 파일 저장 폴더 생성 실패");
+            }
+        }
+        return Paths.get(directoryPath.toString(), fileName);
+    }
+
+    public void saveImageFile(String fileName, MultipartFile imageFile) {
+        Path imagePath = getImageSaveDir(fileName);
+        try {
+            Files.copy(imageFile.getInputStream(), imagePath);
+        } catch (IOException e) {
+            throw new RuntimeException("이미지 파일 저장 실패");
+        }
+    }
+
+    public byte[] getImageByteArray(Image image) {
+        Path filePath = Paths.get(uploadRoot, image.getCreatedDateTime().toLocalDate().toString(), image.getSavedName());
+        try {
+            return Files.readAllBytes(filePath);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void deleteImageFile(Image image) {
+        Path filePath = Paths.get(uploadRoot, image.getCreatedDateTime().toLocalDate().toString(), image.getSavedName());
+        try{
+            Files.delete(filePath);
+        } catch (IOException e){
+            throw new RuntimeException("이미지 파일 삭제 실패");
+        }
+    }
+}
--- src/main/java/com/swyp6/familytravel/image/service/ImageService.java
+++ src/main/java/com/swyp6/familytravel/image/service/ImageService.java
@@ -1,77 +1,55 @@
 package com.swyp6.familytravel.image.service;
+import com.swyp6.familytravel.common.util.ImageSaveUtil;
 import com.swyp6.familytravel.image.entity.Image;
 import com.swyp6.familytravel.image.repository.ImageRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.time.LocalDate;
 import java.util.*;
 
 @Service
+@Transactional
 @RequiredArgsConstructor
 public class ImageService {
 
-    @Value("${images.upload-root}")
-    private String uploadRoot;
-
     private final ImageRepository imageRepository;
+    private final ImageSaveUtil imageSaveUtil;
 
-    private String storeImageFile(MultipartFile imageFile, int order) {
+    private String storeImageFile(MultipartFile imageFile) {
         String originalFileName = Objects.requireNonNull(imageFile.getOriginalFilename());
         String fileExtension = originalFileName.substring(originalFileName.lastIndexOf("."));
         String saveFileName = UUID.randomUUID().toString() + fileExtension;
 
         Image image = Image.builder()
                 .savedName(saveFileName)
-                .order(order)
                 .build();
         imageRepository.save(image);
-
-        Path imageSaveDir = getImageSaveDir();
-        Path imagePath = Paths.get(imageSaveDir.toString(), saveFileName);
-
-        try {
-            Files.copy(imageFile.getInputStream(), imagePath);
-        } catch (IOException e) {
-            throw new RuntimeException("이미지 파일 저장 실패");
-        }
+        imageSaveUtil.saveImageFile(saveFileName, imageFile);
         return saveFileName;
     }
 
     public List<String> storeImageFiles(Optional<List<MultipartFile>> imageFiles) {
-        List<String> result = new ArrayList<>();
-        List<MultipartFile> imageList = imageFiles.orElse(new ArrayList<>());
-        for(int i = 0; i < imageList.size(); i++){
-            result.add(storeImageFile(imageList.get(i), i));
-        }
-        return result;
+        return imageFiles.orElse(new ArrayList<>())
+                .stream()
+                .map(this::storeImageFile)
+                .toList();
     }
 
-    public Path getImageSaveDir(){
-        Path directoryPath = Paths.get(uploadRoot, LocalDate.now().toString());
-        if(Files.notExists(directoryPath)){
-            try{
-                Files.createDirectory(directoryPath);
-            }catch (IOException e){
-                throw new RuntimeException("이미지 파일 저장 폴더 생성 실패");
-            }
-        }
-        return directoryPath;
+    private void deleteImage(String savedName) {
+        Image image = imageRepository.findBySavedName(savedName).orElseThrow(() -> new IllegalArgumentException("해당 이미지가 존재하지 않습니다."));
+        imageRepository.delete(image);
     }
 
+    public void deleteImageList(List<String> savedNames) {
+        savedNames.forEach(this::deleteImage);
+    }
+
+    @Transactional(readOnly = true)
     public byte[] getImageByteArray(String imageId) {
         Image image = imageRepository.findBySavedName(imageId).orElseThrow(() -> new IllegalArgumentException("해당 이미지가 존재하지 않습니다."));
-        Path filePath = Paths.get(uploadRoot, image.getCreatedDateTime().toLocalDate().toString(), image.getSavedName());
-        try {
-            return Files.readAllBytes(filePath);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+        return imageSaveUtil.getImageByteArray(image);
     }
+
 }
