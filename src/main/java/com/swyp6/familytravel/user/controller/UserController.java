343fa81b7eae50f5dc880862e7862ad51dbb87c7
rappire <rappire@gmail.com>
2024-10-03 15:42:30 +0900

    feat: 유저 정보 조회

--- src/main/java/com/swyp6/familytravel/user/controller/UserController.java
+++ src/main/java/com/swyp6/familytravel/user/controller/UserController.java
@@ -1,24 +1,27 @@
 package com.swyp6.familytravel.user.controller;
 
+import com.swyp6.familytravel.auth.entity.CustomUserDetails;
 import com.swyp6.familytravel.auth.jwt.JwtRequestDto;
 import com.swyp6.familytravel.auth.jwt.JwtResponseDto;
 import com.swyp6.familytravel.user.dto.CreateUserDto;
 import com.swyp6.familytravel.user.dto.UpdateUserDto;
 import com.swyp6.familytravel.user.dto.UserDto;
+import com.swyp6.familytravel.user.entity.UserEntity;
 import com.swyp6.familytravel.user.service.UserService;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
-@Controller
+@RestController
 @RequestMapping("/users")
 @RequiredArgsConstructor
+@SecurityRequirement(name = "bearerAuth")
 public class UserController {
 
     private final UserService userService;
@@ -37,5 +40,12 @@ public class UserController {
           @RequestBody UpdateUserDto dto
     ) {
         return userService.updateUser(dto);
+    }
+
+    @PostMapping("/get")
+    public UserEntity getUser(@AuthenticationPrincipal CustomUserDetails customUserDetails ){
+        log.info("getUser");
+        log.info("{}", customUserDetails.getEntity());
+        return customUserDetails.getEntity();
     }
 }
