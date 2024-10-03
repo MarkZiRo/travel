a793ad68f0fc9fb0ec9b2163021ece322c2d7781
rappire <rappire@gmail.com>
2024-10-03 15:42:12 +0900

    feat: CORS 추가

--- src/main/java/com/swyp6/familytravel/auth/config/WebSecurityConfig.java
+++ src/main/java/com/swyp6/familytravel/auth/config/WebSecurityConfig.java
@@ -4,23 +4,28 @@ import com.swyp6.familytravel.auth.filters.JwtTokenFilter;
 import com.swyp6.familytravel.auth.jwt.JwtTokenUtils;
 import com.swyp6.familytravel.auth.oauth.OAuth2SuccessHandler;
 import com.swyp6.familytravel.auth.oauth.OAuth2UserServiceImpl;
-import com.swyp6.familytravel.user.service.JpaUserDetailManager;
 import com.swyp6.familytravel.user.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.access.intercept.AuthorizationFilter;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import java.util.Collections;
+import java.util.List;
 
 @Configuration
 @RequiredArgsConstructor
+@EnableWebSecurity
 public class WebSecurityConfig {
 
     private final JwtTokenUtils jwtTokenUtils;
@@ -28,10 +33,25 @@ public class WebSecurityConfig {
     private final OAuth2UserServiceImpl oAuth2UserService;
     private final OAuth2SuccessHandler oAuth2SuccessHandler;
 
+    @Value("${cors.origin}")
+    private List<String> corsAllowed;
+
+    CorsConfigurationSource corsConfigurationSource() {
+        return request -> {
+            CorsConfiguration config = new CorsConfiguration();
+            config.setAllowedHeaders(Collections.singletonList("*"));
+            config.setAllowedMethods(Collections.singletonList("*"));
+            config.setAllowedOriginPatterns(corsAllowed);
+            config.setAllowCredentials(true);
+            return config;
+        };
+    }
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception
     {
         http
+        .cors(corsConfigure -> corsConfigure.configurationSource(corsConfigurationSource()))
         .csrf(AbstractHttpConfigurer::disable)
         .authorizeHttpRequests(
                 auth -> auth
