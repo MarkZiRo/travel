c1389d8c482be42ea6b6c83f8b4186d30252bfda
rappire <rappire@gmail.com>
2024-10-12 22:13:51 +0900

    fix: DDay 버그 수정

--- src/main/java/com/swyp6/familytravel/travel/repository/TravelRepository.java
+++ src/main/java/com/swyp6/familytravel/travel/repository/TravelRepository.java
@@ -9,5 +9,5 @@ import java.util.Optional;
 
 @Repository
 public interface TravelRepository extends JpaRepository<Travel, Long> {
-    Optional<Travel> findFirstByFamilyIdAndStartDateAfterOrStartDateOrderByStartDateAsc(Long familyId, LocalDate after, LocalDate equal);
+    Optional<Travel> findFirstByFamilyIdAndStartDateGreaterThanEqualOrderByStartDateAsc(Long familyId, LocalDate date);
 }
